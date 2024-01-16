2c27566f43bc1d18e2cdd133cdfff4e9244460f5
beatiemi <beatiemik@gmail.com>
2024-01-16 10:58:23 -0300

    Alteração nos códigos das etapas 1, 2 e 3

--- pyqgis/2_InicializacaoMapa.py
+++ pyqgis/2_InicializacaoMapa.py
@@ -1,54 +1,54 @@
 import requests
-from qgis.core import QgsVectorLayer, QgsVectorLayerJoinInfo
+from qgis.core import QgsVectorLayer
 
-def importar_camada_ottobacias(nome_bd, senha_bd, schema_bd, nome_camada_ottobacias):
+def importar_camada_ottobacias(parametros_conexao, nome_camada_ottobacias):
 #   função de carregamento de camadas vetorial de ottobacias do banco
     uri = QgsDataSourceUri()
-    uri.setConnection(host_bd, porta_bd, nome_bd, usuario_bd, senha_bd)
-    uri.setDataSource(schema_bd, nome_camada_ottobacias, 'geom')
+    uri.setConnection(parametros_conexao['host_bd'], parametros_conexao['porta_bd'], parametros_conexao['nome_bd'], parametros_conexao['usuario_bd'], parametros_conexao['senha_bd'])
+    uri.setDataSource(parametros_conexao['schema_bd'], nome_camada_ottobacias, 'geom')
     ottobacias = QgsVectorLayer(uri.uri(False), 'camada_ottobacias', 'postgres')
     ottobacias.renderer().symbol().setColor(QColor(200, 200, 200, 10))
     QgsProject.instance().addMapLayer(ottobacias)
     print('\n''-> Importação da camada de ottobacias realizada.')
     return ottobacias
 
-def importar_camada_ottotrechos(nome_bd, senha_bd, schema_bd, nome_camada_ottotrechos):
+def importar_camada_ottotrechos(parametros_conexao, schema_bd, nome_camada_ottotrechos):
 #   função de carregamento de camadas vetorial de ottotrechos do banco
     uri = QgsDataSourceUri()
-    uri.setConnection(host_bd, porta_bd, nome_bd, usuario_bd, senha_bd)
-    uri.setDataSource(schema_bd, nome_camada_ottotrechos, "geom")
+    uri.setConnection(parametros_conexao['host_bd'], parametros_conexao['porta_bd'], parametros_conexao['nome_bd'], parametros_conexao['usuario_bd'], parametros_conexao['senha_bd'])
+    uri.setDataSource(parametros_conexao['schema_bd'], nome_camada_ottotrechos, 'geom')
     ottotrechos = QgsVectorLayer(uri.uri(False), 'camada_ottotrechos', 'postgres')
     ottotrechos.renderer().symbol().setColor(QColor(0, 150, 255))
     QgsProject.instance().addMapLayer(ottotrechos)
     print('\n''-> Importação da camada de ottotrechos realizada.')
     return ottotrechos
 
-def importar_disponibilidade_hidrica(nome_bd, senha_bd, nome_camada_disp):
+def importar_disponibilidade_hidrica(parametros_conexao, schema_bd, nome_camada_disp):
 #   função de carregamento de camadas vetorial de disponibilidade hídrica do banco
     uri = QgsDataSourceUri()
-    uri.setConnection(host_bd, porta_bd, nome_bd, usuario_bd, senha_bd)
-    uri.setDataSource(schema_bd, nome_camada_disp, 'geom')
+    uri.setConnection(parametros_conexao['host_bd'], parametros_conexao['porta_bd'], parametros_conexao['nome_bd'], parametros_conexao['usuario_bd'], parametros_conexao['senha_bd'])
+    uri.setDataSource(parametros_conexao['schema_bd'], nome_camada_disp, 'geom')
     disponibilidade_hidrica = QgsVectorLayer(uri.uri(False), 'camada_disp_hid', 'postgres')
     QgsProject.instance().addMapLayer(disponibilidade_hidrica, False)  # Camada adicionada, mas não visível.
     print('\n''-> Importação da camada de disponibilidade hídrica realizada.')
     return disponibilidade_hidrica
 
-def importar_captacoes(nome_bd, senha_bd):
+def importar_captacoes(parametros_conexao, nome_camada_outorgas):
 #   função de carregamento de camadas vetorial de outorgas de captação do banco
     uri = QgsDataSourceUri()
-    uri.setConnection(host_bd, porta_bd, nome_bd, usuario_bd, senha_bd)
-    uri.setDataSource(schema_bd, 'CNARH_Estadual_ANA_2022', 'geom')
+    uri.setConnection(parametros_conexao['host_bd'], parametros_conexao['porta_bd'], parametros_conexao['nome_bd'], parametros_conexao['usuario_bd'], parametros_conexao['senha_bd'])
+    uri.setDataSource(parametros_conexao['schema_bd'], nome_camada_outorgas, 'geom')
     outorgas = QgsVectorLayer(uri.uri(False), 'camada_outorgas', 'postgres')
-    QgsProject.instance().addMapLayer(outorgas, True)  # Camada adiciona, mas não visível.
+    QgsProject.instance().addMapLayer(outorgas, True) 
     print('\n''-> Importação da camada de captações realizada.')
     return outorgas
-    
+
 def importar_camada_fundo():
 #   função de carregamento da camada de plano de fundo
     service_url = 'mt1.google.com/vt/lyrs=m&x={x}&y={y}&z={z}'
     service_uri = 'type=xyz&zmin=0&zmax=21&url=https://'+requests.utils.quote(service_url)
     iface.addRasterLayer(service_uri, 'Google_Road', 'wms')
-    
+
 ### EXECUÇÃO ###
 
 # IMPORTAÇÃO DE CAMADA DE FUNDO #
@@ -58,6 +58,8 @@ print('\n''-> Camada de fundo adicionada.')
 # IMPORTAÇÃO CAMADAS DA BACIA DE INTERESSE #
 nome_camada_ottobacias = 'ottobacias_pb_5k'
 nome_camada_ottotrechos = 'ottotrechos_pb_5k'
-ottobacias = importar_camada_ottobacias(nome_bd, senha_bd, schema_bd, nome_camada_ottobacias)
-ottotrechos = importar_camada_ottotrechos(nome_bd, senha_bd, schema_bd, nome_camada_ottotrechos)
+nome_camada_disp = 'disp_hid_pb_5k'
+nome_camada_outorgas = 'outorgas_pb'
+ottobacias = importar_camada_ottobacias(parametros_conexao, nome_camada_ottobacias)
+ottotrechos = importar_camada_ottotrechos(parametros_conexao, nome_camada_ottotrechos)
 print('\n''-> Seleção das camadas da bacia realizada.')
--- pyqgis/3_DefinicaoVazaoCaptacao.py
+++ pyqgis/3_DefinicaoVazaoCaptacao.py
@@ -1,9 +1,12 @@
-def agregacao_vazao_captacao(outorgas, ottobacias_montante):
+from qgis.core import QgsExpressionContext, QgsExpressionContextUtils
+from qgis import processing
+
+def agregacao_vazao_captacao(outorgas, ottobacias):
     # método que realiza operações para obter o valor da vazão nas ottobacias a montante da bacia de interesse
     processo_bacias_outorgas = processing.run("native:intersection",{
                                                     'INPUT': outorgas,
-                                                    'OVERLAY':ottobacias_montante,
-                                                    'OUTPUT':'memory: outorgas_e_ottobacias_montante'})
+                                                    'OVERLAY':ottobacias,
+                                                    'OUTPUT':'memory: outorgas_e_ottobacias'})
     intersecao_bacias_outorgas = processo_bacias_outorgas['OUTPUT']
         
     context = QgsExpressionContext()
@@ -12,17 +15,35 @@ def agregacao_vazao_captacao(outorgas, ottobacias_montante):
     processo_de_agrupamento_por_ottobacias = processing.run("native:aggregate", {
                 'INPUT': intersecao_bacias_outorgas,
 		        'GROUP_BY':'"cobacia"',
-		        'AGGREGATES':[{'aggregate': 'first_value','delimiter': ',','input': '"cobacia"','length': 0,'name': 'cobacia','precision': 0,'sub_type': 0,'type': 10,'type_name': 'text'},
-                            {'aggregate': 'sum','delimiter': ',','input': '"vazao"','length': 0,'name': 'vazao','precision': 0,'sub_type': 0,'type': 6,'type_name': 'double precision'}],
-		        'OUTPUT':'memory: ottobacias_montante_vazao'})
+		        'AGGREGATES':[{'aggregate': 'first_value',
+                                'delimiter': ',',
+                                'input': '"cobacia"',
+                                'length': 0,
+                                'name': 'cobacia',
+                                'precision': 0,
+                                'sub_type': 0,
+                                'type': 10,
+                                'type_name': 'text'},
+                            {'aggregate': 'sum',
+                                'delimiter': ',',
+                                'input': '"vazão horária (m³/h)"',
+                                'length': 0,
+                                'name': 'vazão horária (m³/h)',
+                                'precision': 0,
+                                'sub_type': 0,
+                                'type': 6,
+                                'type_name': 'double precision'}],
+		        'OUTPUT':'memory: ottobacias_vazao'})
     agrupamento_por_ottobacias = processo_de_agrupamento_por_ottobacias['OUTPUT']
+    QgsProject.instance().addMapLayer(agrupamento_por_ottobacias, True)
     return intersecao_bacias_outorgas, agrupamento_por_ottobacias
 
 ### EXECUÇÃO ###
 
 # IMPORTAÇÃO DE CAMADAS DE OUTORGAS #
-outorgas = importar_outorgas(nome_bd, senha_bd)
+outorgas = importar_captacoes(parametros_conexao['host_bd'], parametros_conexao['nome_bd'], parametros_conexao['usuario_bd'], parametros_conexao['senha_bd'], parametros_conexao['porta_bd'], parametros_conexao['schema_bd'], nome_camada_outorgas)
 
 # INTERSEÇÃO DE OTTOBACIAS E OUTORGAS DE CAPTAÇÃO, AGREGAÇÃO DAS VAZÕES DE CAPTAÇÃO #
-intersecao_bacias_outorgas, agrupamento_por_ottobacias = agregacao_vazao_captacao(outorgas, ottobacias_montante)
+intersecao_bacias_outorgas, agrupamento_por_ottobacias = agregacao_vazao_captacao(outorgas, ottobacias)
 print('\n''-> Insterseção de ottobacias e outorgas de captação e agregação das vazões realizada.')
+
