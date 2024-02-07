baeff0fa92d6b7dfb110d7e44de925d23731f3f2
brunoguterres <brunoguterres26@gmail.com>
2024-02-07 14:04:29 -0300

    refatoração de ajustes

--- pyqgis/2_InicializacaoMapa.py
+++ pyqgis/2_InicializacaoMapa.py
@@ -1,7 +1,7 @@
 import requests
 
 def importar_camada_ottobacias(parametros_conexao, nome_camada_ottobacias):
-#   função de carregamento de camadas vetorial de ottobacias do banco
+    # função de carregamento de camada vetorial de ottobacias do banco
     uri = QgsDataSourceUri()
     uri.setConnection(parametros_conexao['host_bd'],
                       parametros_conexao['porta_bd'],
@@ -10,13 +10,15 @@ def importar_camada_ottobacias(parametros_conexao, nome_camada_ottobacias):
                       parametros_conexao['senha_bd'])
     uri.setDataSource(parametros_conexao['schema_bd'], nome_camada_ottobacias, 'geom')
     ottobacias = QgsVectorLayer(uri.uri(False), 'camada_ottobacias', 'postgres')
-    ottobacias.renderer().symbol().setColor(QColor(200, 200, 200, 10))
-    QgsProject.instance().addMapLayer(ottobacias)
     print('\n''-> Importação da camada de ottobacias realizada.')
-    return ottobacias
+    simbologia_ottobacias = {'r':200,
+                             'g':200,
+                             'b':200,
+                             'a':10}
+    return ottobacias, simbologia_ottobacias
 
 def importar_camada_ottotrechos(parametros_conexao, nome_camada_ottotrechos):
-#   função de carregamento de camadas vetorial de ottotrechos do banco
+    # função de carregamento de camada vetorial de ottotrechos do banco
     uri = QgsDataSourceUri()
     uri.setConnection(parametros_conexao['host_bd'],
                       parametros_conexao['porta_bd'],
@@ -25,29 +27,36 @@ def importar_camada_ottotrechos(parametros_conexao, nome_camada_ottotrechos):
                       parametros_conexao['senha_bd'])
     uri.setDataSource(parametros_conexao['schema_bd'], nome_camada_ottotrechos, 'geom')
     ottotrechos = QgsVectorLayer(uri.uri(False), 'camada_ottotrechos', 'postgres')
-    ottotrechos.renderer().symbol().setColor(QColor(0, 150, 255))
-    QgsProject.instance().addMapLayer(ottotrechos)
     print('\n''-> Importação da camada de ottotrechos realizada.')
-    return ottotrechos
+    simbologia_ottotrechos = {'r':0,
+                              'g':150,
+                              'b':255,
+                              'a':255}
+    return ottotrechos, simbologia_ottotrechos
+
+def carregar_camada(camada, simbologia):
+    camada.renderer().symbol().setColor(QColor(simbologia['r'],
+                                               simbologia['g'],
+                                               simbologia['b'],
+                                               simbologia['a']))
+    QgsProject.instance().addMapLayer(camada)
+    print('\n''-> Carregamento de "'+camada.name()+'" realizado.')
+
 
 def importar_camada_fundo():
-#   função de carregamento da camada de plano de fundo
+    # função de carregamento da camada de plano de fundo
     service_url = 'mt1.google.com/vt/lyrs=m&x={x}&y={y}&z={z}'
     service_uri = 'type=xyz&zmin=0&zmax=21&url=https://'+requests.utils.quote(service_url)
     iface.addRasterLayer(service_uri, 'Google_Road', 'wms')
+    print('\n''-> Camada de fundo adicionada.')
 
 ### EXECUÇÃO ###
 
-# IMPORTAÇÃO DE CAMADA DE FUNDO #
 importar_camada_fundo()
-print('\n''-> Camada de fundo adicionada.')
 
-# IMPORTAÇÃO CAMADAS DA BACIA DE INTERESSE #
 nome_camada_ottobacias = 'ottobacias_pb_5k'
 nome_camada_ottotrechos = 'ottotrechos_pb_5k'
-nome_camada_disp = 'disp_hid_pb_5k'
-nome_camada_outorgas = 'outorgas_pb'
-ottobacias = importar_camada_ottobacias(parametros_conexao, nome_camada_ottobacias)
-ottotrechos = importar_camada_ottotrechos(parametros_conexao, nome_camada_ottotrechos)
-disponibilidade = importar_disponibilidade_hidrica(parametros_conexao, nome_camada_disp)
-print('\n''-> Seleção das camadas da bacia realizada.')
+ottobacias, simbologia_ottobacias = importar_camada_ottobacias(parametros_conexao, nome_camada_ottobacias)
+ottotrechos, simbologia_ottotrechos = importar_camada_ottotrechos(parametros_conexao, nome_camada_ottotrechos)
+carregar_camada(ottobacias, simbologia_ottobacias)
+carregar_camada(ottotrechos, simbologia_ottotrechos)
