9bf6b7636181e84d1ee4b5eb388947213ba1a83b
Bruno Guterres <brunoguterres26@gmail.com>
2024-02-08 08:41:54 -0300

    refatoração de testes

--- pyqgis/5_CalculoBalanco.py
+++ pyqgis/5_CalculoBalanco.py
@@ -1,15 +1,16 @@
-def criar_campos(matriz):   # esta função cria campos novos na matriz que serão necessários no cálculo do balanço
+def criar_campos(matriz):
     for linha in matriz:
-        linha.append(0)  # adiciona um atributo "0" no final de cada linha da linha - campo_vazao_montante
-        linha.append(0)  # adiciona um atributo "0" no final de cada linha da linha - campo_vazao_jusante
+        linha.append(0)
+        linha.append(0)
+        linha.append(0)
     return matriz
 
-def calculo_balanco(matriz): #esta função realiza o cálculo do balanço
+def calcular_balanco(matriz):
     for i in range(len(matriz)):
-        if matriz[i][campo_cabeceira] == '1':
+        if matriz[i][campo_cabeceira] == True:
             pass
         else:
-            for j in range(i-1,-1,-1):
+            for j in range(i-1,0,-1):
                 contador_montante = 0
                 if matriz[i][campo_cotrecho] == matriz[j][campo_trechojus] :
                     a = float(matriz[i][campo_vazao_montante])
@@ -18,21 +19,20 @@ def calculo_balanco(matriz): #esta função realiza o cálculo do balanço
                     if contador_montante == 2:
                         break
         matriz[i][campo_vazao_jusante] = float(matriz[i][campo_vazao_montante]) + float(matriz[i][campo_disponibilidade])
-    return matriz.tolist()
+    return matriz
+
+def print_matriz(matriz):
+    for row in matriz:
+        print(" ".join(map(str, row)))
 
 
 ### EXECUÇÃO ###
 
 matriz = []
-campos = trecho_disponibilidade_captacao.fields()
-matriz.append([campo.name() for campo in campos])
-
 for feicao in trecho_disponibilidade_captacao.getFeatures():
     matriz.append([feicao[campo.name()] for campo in campos])
 
 matriz = criar_campos(matriz)
-print(matriz)
-
 
 campo_cobacia = 0
 campo_cotrecho = 1
@@ -44,7 +44,8 @@ campo_vazao_montante = 6
 campo_vazao_jusante = 7
 campo_deficit = 8
 
-'''
-resultado_balanco = calculo_balanco(matriz)
-'''
+resultado_balanco = calcular_balanco(matriz)
+
+print_matriz(resultado_balanco)
+
 print('\n''-> Cálculo do balanço hídrico realizado.')
