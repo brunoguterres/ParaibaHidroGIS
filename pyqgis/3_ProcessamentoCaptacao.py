83868848e97e573ff444664b01e62da2034f9166
brunoguterres <brunoguterres26@gmail.com>
2024-04-03 15:13:17 -0300

    edição de correção e atualização

--- pyqgis/3_ProcessamentoCaptacao.py
+++ pyqgis/3_ProcessamentoCaptacao.py
@@ -13,14 +13,14 @@ def processamento_captacoes():
         DROP VIEW IF EXISTS cenario_0.captacoes_ottobacias CASCADE;
         CREATE VIEW cenario_0.captacoes_ottobacias AS
         SELECT
-	        cenario_0.ottobacias_5k.cobacia,
+	        basemap.ottobacias_pb_5k.cobacia,
 	        SUM(cenario_0.outorgas.captacao_solicitada) AS captacao_solicitada
         FROM cenario_0.outorgas
         JOIN basemap.ottobacias_pb_5k
-        ON ST_Intersects(cenario_0.outorgas.geom, cenario_0.ottobacias_5k.geom)
+        ON ST_Intersects(cenario_0.outorgas.geom, basemap.ottobacias_pb_5k.geom)
         GROUP BY basemap.ottobacias_pb_5k.cobacia
         ORDER BY basemap.ottobacias_pb_5k.cobacia DESC;
-    """)
+        """)
 
     conexao.commit()
     cursor.close()
@@ -29,18 +29,5 @@ def processamento_captacoes():
 
 ### EXECUÇÃO ###
 
-'''
-nome_tabela_captacoes = 'outorgas'
-nome_tabela_captacao_ottobacia = 'captacao_ottobacia'
-nome_tabela_disponibilidade = 'disp_hid_pb_5k'
-captacao_ottobacia = importar_camada_bdg(nome_tabela_bdg=nome_tabela_captacao_ottobacia, nome_camada='camada_captacao_ottobacia')
-disponibilidade = importar_camada_bdg(nome_tabela_bdg=nome_tabela_disponibilidade, nome_camada='camada_disponibilidade')
-simbologia_disponibilidade = {'r':0, 'g':255, 'b':0, 'a':255}
-simbologia_captacao_ottobacia = {'r':255, 'g':180, 'b':0, 'a':255}
-carregar_camada(disponibilidade, simbologia_disponibilidade)
-carregar_camada(captacao_ottobacia, simbologia_captacao_ottobacia)
-'''
-
 processamento_captacoes()
-
-print('--> Definições de disponibilidades e captações realizada.')
+print('--> Processamento de captações realizado.')
