e496507f0d77227d58fba77219f70df63ed7a60f
brunoguterres <brunoguterres26@gmail.com>
2024-04-24 17:22:06 -0300

    refatoração de atualização de simbologia da etapa 2

--- pyqgis/2_InicializacaoMapa.py
+++ pyqgis/2_InicializacaoMapa.py
@@ -26,17 +26,27 @@ def importar_camada_bdg(nome_tabela_bdg, nome_schema_bdg, nome_camada):
     return camada_importada
 
 def carregar_camada(camada, simbologia):
-    camada.renderer().symbol().setColor(QColor(simbologia['r'],
-                                               simbologia['g'],
-                                               simbologia['b'],
-                                               simbologia['a']))
+    tipo_geometria = camada.geometryType()
+    if tipo_geometria == QgsWkbTypes.PointGeometry:
+        symbol = QgsSimpleMarkerSymbolLayer.create({
+            'color': simbologia['cor'],
+            'size': simbologia['tamanho']
+        })
+    elif tipo_geometria == QgsWkbTypes.LineGeometry:
+        symbol = QgsLineSymbol.createSimple({
+            'color': simbologia['cor'],
+            'width': simbologia['espessura']
+        })
+    elif tipo_geometria == QgsWkbTypes.PolygonGeometry:
+        symbol = QgsFillSymbol.createSimple({
+            'color': simbologia['cor_preenchimento'],
+            'color_border': simbologia['cor_contorno'],
+            'width_border': simbologia['espessura_contorno']
+        })
+    renderer = QgsSingleSymbolRenderer(symbol)
+    camada.setRenderer(renderer)
     QgsProject.instance().addMapLayer(camada)
     print(f'--> Carregamento de "{camada.name()}" realizado.')
-
-    if camada.geometryType() == QgsWkbTypes.PolygonGeometry:
-        print("A camada é do tipo polígono.")
-    else:
-        print("A camada não é do tipo polígono.")
 
 def importar_camada_fundo():
     # função de carregamento da camada de plano de fundo
@@ -53,8 +63,11 @@ nome_tabela_ottobacias = 'ottobacias_pb_5k'
 nome_tabela_ottotrechos = 'ottotrechos_pb_5k'
 ottobacias = importar_camada_bdg(nome_tabela_bdg=nome_tabela_ottobacias, nome_schema_bdg=basemap, nome_camada='camada_ottobacias')
 ottotrechos = importar_camada_bdg(nome_tabela_bdg=nome_tabela_ottotrechos, nome_schema_bdg=basemap, nome_camada='camada_ottotrechos')
-simbologia_ottobacias = {'r':200, 'g':200, 'b':200, 'a':10}
-simbologia_ottotrechos = {'r':0, 'g':150, 'b':255, 'a':255}
+simbologia_ottobacias = {'cor_preenchimento': QColor(200, 200, 200, 10),
+                         'cor_contorno': QColor(0, 0, 0, 255),
+                         'espessura_contorno': 0.05}
+simbologia_ottotrechos = {'cor': QColor(0, 150, 255, 255),
+                          'espessura': 0.3}
 importar_camada_fundo()
 carregar_camada(ottobacias, simbologia_ottobacias)
 carregar_camada(ottotrechos, simbologia_ottotrechos)
