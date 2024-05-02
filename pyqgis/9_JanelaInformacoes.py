677f20f9e9d7671bfec51b93f101077ea1fdc274
brunoguterres <brunoguterres26@gmail.com>
2024-05-02 09:25:24 -0300

    edição de atualização

--- pyqgis/9_JanelaInformacoes.py
+++ pyqgis/9_JanelaInformacoes.py
@@ -1,7 +1,7 @@
 def apresenta_informacoes(informacoes_selecao):
-    verifica_postgis = QMessageBox()
-    verifica_postgis.setWindowTitle('Resultado')
-    verifica_postgis.setText('Informações sobre a área selecionado'
+    janela_informacoes = QMessageBox()
+    janela_informacoes.setWindowTitle('Resultado')
+    janela_informacoes.setText('Informações sobre a área selecionado'
                            '\n'
                            '\n NOME DO RIO: ' + str(informacoes_selecao['nome_rio']) +
                            '\n ÁREA À MONTANTE: ' + str(informacoes_selecao['area_mont']) +
@@ -9,15 +9,12 @@ def apresenta_informacoes(informacoes_selecao):
                            '\n ISR DA OTTOBACIA: ' + str(informacoes_selecao['isr_otto']) +
                            '\n VAZÃO CAPTADA À MONTANTE: ' + str(informacoes_selecao['q_cap_mont']) +
                            '\n DISPONIBILIDADE À MONTANTE: ' + str(informacoes_selecao['disp_acum']))
-    verifica_postgis.setStandardButtons(QMessageBox.Yes | QMessageBox.No)
-    return_value = verifica_postgis.exec()
-    if return_value == QMessageBox.No:
-        parametros_conexao = parametros_personalizados_bd(parametros_conexao)
-    return parametros_conexao
+    #verifica_postgis.setStandardButtons(QMessageBox.Yes | QMessageBox.No)
+    janela_informacoes.exec()
 
 def obter_informacoes():
     informacoes_selecao = {'nome_rio':'RIO',
-                           'area_montante':9999,
+                           'area_mont':9999,
                            'n_trechos_mont':9999,
                            'isr_otto':'X',
                            'q_cap_mont':9999,
