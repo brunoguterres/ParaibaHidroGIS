fb62f7dceeb989b95e9ff15f1f0e73f0f1017901
brunoguterres <brunoguterres26@gmail.com>
2024-05-24 11:49:14 -0300

    atualização de código

--- pyqgis/8_RepresentacaoSelecao_v2.py
+++ pyqgis/8_RepresentacaoSelecao_v2.py
@@ -1,5 +1,32 @@
 import psycopg2
 
+
+def carregar_camada(camada, simbologia):
+    tipo_geometria = camada.geometryType()
+    if tipo_geometria == QgsWkbTypes.PointGeometry:
+        symbol = QgsSimpleMarkerSymbolLayer.create({
+            'color': simbologia['cor'],
+            'size': simbologia['tamanho']
+        })
+    elif tipo_geometria == QgsWkbTypes.LineGeometry:
+        symbol = QgsLineSymbol.createSimple({
+            'color': simbologia['cor'],
+            'width': simbologia['espessura']
+        })
+    elif tipo_geometria == QgsWkbTypes.PolygonGeometry:
+        symbol = QgsFillSymbol.createSimple({
+            'color': simbologia['cor_preenchimento'],
+            'color_border': simbologia['cor_contorno'],
+            'width_border': simbologia['espessura_contorno']
+        })
+    renderer = QgsSingleSymbolRenderer(symbol)
+    camada.setRenderer(renderer)
+    QgsProject.instance().addMapLayer(camada)
+    print(f'--> Carregamento de "{camada.name()}" realizado.')
+
+
+
+
 conexao = psycopg2.connect(
 dbname = str(parametros_conexao['nome_bd']),
 user = str(parametros_conexao['usuario_bd']),
@@ -37,13 +64,38 @@ cursor.execute(f'''
 ''')
 conexao.commit()
 
+
+
 cursor.execute(f'''
-    DROP VIEW IF EXISTS cenario_2.bacia_montante CASCADE;
-    CREATE VIEW cenario_2.bacia_montante AS
+    DROP VIEW IF EXISTS {parametros_conexao['schema_cenario']}.bacia_montante CASCADE;
+    CREATE VIEW {parametros_conexao['schema_cenario']}.bacia_montante AS
     SELECT ST_UNION(geom) as geom
-    FROM cenario_2.ottobacias_isr_montante;
+    FROM {parametros_conexao['schema_cenario']}.ottobacias_isr_montante;
 ''')
 conexao.commit()
+
+
+
+# solução provisória
+uri = QgsDataSourceUri()
+uri.setConnection(parametros_conexao['host_bd'],
+                    parametros_conexao['porta_bd'],
+                    parametros_conexao['nome_bd'],
+                    parametros_conexao['usuario_bd'],
+                    parametros_conexao['senha_bd'])
+uri.setDataSource(parametros_conexao['schema_cenario'], 'ottobacias_isr_montante', 'geom', '', 'cobacia')
+ottobacias_isr_montante = QgsVectorLayer(uri.uri(False), 'camada_ottobacias_isr_montante', 'postgres')
+QgsProject.instance().addMapLayer(ottobacias_isr_montante)
+simbologia_ottobacias_isr_montante = {'cor_preenchimento': QColor(0, 0, 0, 0),
+                                        'cor_contorno': QColor(0, 0, 0, 0),
+                                        'espessura_contorno': 1}
+carregar_camada(ottobacias_isr_montante, simbologia_ottobacias_isr_montante)
+print(f'--> Importação da camada "{ottobacias_isr_montante.name()}" realizada.')
+bacia_montante = QgsVectorLayer(f'VirtualLayer?query=SELECT ST_UNION(geometry) FROM camada_ottobacias_isr_montante','camada_bacia_montante', 'virtual')
+
+
+
+
 
 # inicio da plotagem do trecho de jusante - apenas o jusante 1 no estilo antigo
 # calculo dos rios que estão a jusante
@@ -106,17 +158,29 @@ if QgsProject.instance().mapLayersByName('camada_ottobacias_montante'):
 
 
 
-
+"""
 uri = QgsDataSourceUri()
 uri.setConnection(parametros_conexao['host_bd'],
                   parametros_conexao['porta_bd'],
                   parametros_conexao['nome_bd'],
                   parametros_conexao['usuario_bd'],
                   parametros_conexao['senha_bd'])
-uri.setDataSource(parametros_conexao['schema_cenario'], 'bacia_montante', 'geom')
+uri.setDataSource(parametros_conexao['schema_cenario'], 'bacia_montante', 'geom', '', 'geom')
 bacia_montante = QgsVectorLayer(uri.uri(), 'camada_bacia_montante', 'postgres')
 QgsProject.instance().addMapLayer(bacia_montante)
 print(f'--> Carregamento de "{bacia_montante.name()}" realizado.')
+"""
+# solução provisória
+simbologia_bacia_montante = {'cor_preenchimento': QColor(0, 0, 0, 0),
+                            'cor_contorno': QColor(255, 0, 0, 255),
+                            'espessura_contorno': 1}
+carregar_camada(bacia_montante, simbologia_bacia_montante)
+print(f'--> Carregamento de "{bacia_montante.name()}" realizado.')
+
+
+
+
+
 
 uri = QgsDataSourceUri()
 uri.setConnection(parametros_conexao['host_bd'],
